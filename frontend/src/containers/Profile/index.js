86e5b0d0dbc5a82fc41a8298014bd15cfa30716d
tset0401 <tset0401@gmail.com>
2017-12-13 23:59:04 +0700

    fix

--- frontend/src/containers/Profile/index.js
+++ frontend/src/containers/Profile/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import DatePicker from 'react-datepicker';
 
 class Profile extends Component {
     constructor (props) {
