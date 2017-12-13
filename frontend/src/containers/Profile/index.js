4441e9d08918890ef5cb40577d317428091b0e2e
tset0401 <tset0401@gmail.com>
2017-12-13 23:32:22 +0700

    add user info

--- frontend/src/containers/Profile/index.js
+++ frontend/src/containers/Profile/index.js
@@ -10,7 +10,7 @@ class Profile extends Component {
         };
     }
     change = (e) => {
-        this.setState({ [e.target.name]: e.target.value });
+        this.setState({...this.state, [e.target.name]: e.target.value });
     }
     render() {
         return (
@@ -23,37 +23,42 @@ class Profile extends Component {
             <form className="form-horizontal">
                 {
                 Object.keys(this.props.account).map((attr, index) => {
-                    return (
-                        <div className="form-group" key={index}>
-                            <label htmlFor={attr}>{attr.toUpperCase()}</label>
-                            <input className="form-control" id={attr} name={attr} value={this.props.account[attr]?this.props.account[attr]:""} onChange={this.change}/>
-                        </div>
-                    );
+                    if (attr == "accountId" || attr == "role"){
+                        return;
+                    }
+                    if (attr == "password"){
+                        return (
+                            <div className="control-group" key={index}>
+                                <label className="control-label" htmlFor={attr}><b>{attr.toUpperCase()}</b></label>
+                                <div className="controls">
+                                    <input id={attr} name={attr} value="********" disabled="disabled"/>
+                                </div>
+                            </div>
+                        )
+                    } else if (attr == "userName"){
+                        return (
+                            <div className="control-group" key={index}>
+                                <label className="control-label" htmlFor={attr}><b>{attr.toUpperCase()}</b></label>
+                                <div className="controls">
+                                    <input id={attr} name={attr} value={this.props.account[attr]?this.props.account[attr]:""} disabled="disabled"/>
+                                </div>
+                            </div>
+                        )
+                    } else {
+                        return (
+                            <div className="control-group" key={index}>
+                                <label className="control-label" htmlFor={attr}><b>{attr.toUpperCase()}</b></label>
+                                <div className="controls">
+                                    <input id={attr} name={attr} value={this.props.account[attr]?this.props.account[attr]:""} onChange={this.change}/>
+                                </div>
+                            </div>
+                        );
+                    }
                 })
                 }
-                {/* <div className="form-group">
-                    <label className="control-label col-sm-2" htmlFor="email">Email:</label>
-                    <div className="col-sm-10">
-                        <input type="email" className="form-control" id="email" placeholder="Enter email" name="email"/>
-                    </div>
-                    </div>
-                    <div className="form-group">
-                    <label className="control-label col-sm-2" htmlFor="pwd">Password:</label>
-                    <div className="col-sm-10">          
-                        <input type="password" className="form-control" id="pwd" placeholder="Enter password" name="pwd"/>
-                    </div>
-                    </div>
-                    <div className="form-group">        
-                    <div className="col-sm-offset-2 col-sm-10">
-                        <div className="checkbox">
-                        <label><input type="checkbox" name="remember"/> Remember me</label>
-                        </div>
-                    </div>
-                    </div>
-                 */}
                  <div className="form-group">
                     <div className="col-sm-offset-2 col-sm-10">
-                        <button type="submit" className="btn btn-default">Submit</button>
+                        <button type="submit" className="btn btn-default">Update</button>
                     </div>
                 </div>
             </form>
